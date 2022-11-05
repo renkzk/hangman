1d6a14273b16e0989cc46635271e6cebf24fdcc8
renhubgit <renik2001@gmail.com>
2022-11-05 13:38:33 +0100

    added main app components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import { useState } from "react"
+import styled from "styled-components"
 import words from "./wordList.json"
+import Hangman from "./components/Hangman"
+import Word from "./components/Word"
+import Keyboard from "./components/Keyboard"
 
 function App() {
   const [wordToGuess, setWordToGuess] = useState(() => {
@@ -8,7 +12,22 @@ function App() {
 
   const [guessedLetters, setGuessedLetters] = useState<string[]>([])
 
-  return <h1>{wordToGuess}</h1>
+  return (
+    <AppContainer>
+      <div className="game-info">You Won.</div>
+      <Hangman />
+      <Word />
+      <Keyboard />
+    </AppContainer>
+  )
 }
+
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  margin: 0 auto;
+  align-items: center;
+`
 
 export default App
