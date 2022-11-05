2e3f60b269e9355d9d32d378aa004cc66689faac
renhubgit <renik2001@gmail.com>
2022-11-05 16:54:19 +0100

    hangman bar styling

--- src/components/Hangman.tsx
+++ src/components/Hangman.tsx
@@ -1,5 +1,44 @@
-import React from "react"
+import styled from "styled-components"
 
 export default function Hangman() {
-  return <div>Hangman</div>
+  return (
+    <HangmanContainer>
+      <div className="top-bar" />
+      <div className="rope-bar" />
+      <div className="middle-bar" />
+      <div className="bottom-bar" />
+    </HangmanContainer>
+  )
 }
+
+const HangmanContainer = styled.div`
+  position: relative;
+
+  .top-bar {
+    height: 10px;
+    width: 250px;
+    background-color: black;
+    margin-left: 50%;
+  }
+
+  .rope-bar {
+    height: 50px;
+    width: 10px;
+    background-color: black;
+    position: absolute;
+    right: -50%;
+  }
+
+  .middle-bar {
+    height: 400px;
+    width: 10px;
+    background-color: black;
+    margin-left: 50%;
+  }
+
+  .bottom-bar {
+    height: 10px;
+    width: 250px;
+    background-color: black;
+  }
+`
