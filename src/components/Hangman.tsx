2adb574edf0488f2294115b78870cd761a55ed70
renhubgit <renik2001@gmail.com>
2022-11-05 17:26:52 +0100

    added hangman elements and styling

--- src/components/Hangman.tsx
+++ src/components/Hangman.tsx
@@ -1,8 +1,94 @@
 import styled from "styled-components"
 
 export default function Hangman() {
+  const HEAD = (
+    <div
+      style={{
+        width: "50px",
+        height: "50px",
+        borderRadius: "100%",
+        border: "10px solid black",
+        position: "absolute",
+        right: "-50%",
+        top: "50px",
+      }}
+    />
+  )
+
+  const BODY = (
+    <div
+      style={{
+        width: "10px",
+        height: "100px",
+        background: "black",
+        position: "absolute",
+        right: "-38%",
+        top: "120px",
+      }}
+    />
+  )
+
+  const LEFT_ARM = (
+    <div
+      style={{
+        width: "100px",
+        height: "10px",
+        background: "black",
+        position: "absolute",
+        right: "-68%",
+        top: "160px",
+        rotate: "50deg",
+      }}
+    />
+  )
+  const RIGHT_ARM = (
+    <div
+      style={{
+        width: "100px",
+        height: "10px",
+        background: "black",
+        position: "absolute",
+        right: "-44%",
+        top: "160px",
+        rotate: "-50deg",
+      }}
+    />
+  )
+  const LEFT_LEG = (
+    <div
+      style={{
+        width: "120px",
+        height: "10px",
+        background: "black",
+        position: "absolute",
+        right: "-68%",
+        top: "260px",
+        rotate: "70deg",
+      }}
+    />
+  )
+  const RIGHT_LEG = (
+    <div
+      style={{
+        width: "120px",
+        height: "10px",
+        background: "black",
+        position: "absolute",
+        right: "-52%",
+        top: "260px",
+        rotate: "-70deg",
+      }}
+    />
+  )
+
   return (
     <HangmanContainer>
+      {HEAD}
+      {BODY}
+      {LEFT_ARM}
+      {RIGHT_ARM}
+      {LEFT_LEG}
+      {RIGHT_LEG}
       <div className="top-bar" />
       <div className="rope-bar" />
       <div className="middle-bar" />
@@ -13,10 +99,11 @@ export default function Hangman() {
 
 const HangmanContainer = styled.div`
   position: relative;
+  margin-left: -20%;
 
   .top-bar {
     height: 10px;
-    width: 250px;
+    width: 220px;
     background-color: black;
     margin-left: 50%;
   }
@@ -26,7 +113,7 @@ const HangmanContainer = styled.div`
     width: 10px;
     background-color: black;
     position: absolute;
-    right: -50%;
+    right: -38%;
   }
 
   .middle-bar {
