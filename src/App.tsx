7e8d49337f4eeaec173b8b22a88f42e6ce745216
renhubgit <renik2001@gmail.com>
2022-11-05 13:27:28 +0100

    added state to manage guessed letters

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ function App() {
   const [wordToGuess, setWordToGuess] = useState(() => {
     return words[Math.floor(Math.random() * words.length)]
   })
+
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
   return <h1>{wordToGuess}</h1>
 }
 
