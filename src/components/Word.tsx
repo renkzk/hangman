c51b798eed823ddb0cafd1b5bde0fa1057b3ecdc
renhubgit <renik2001@gmail.com>
2022-11-05 19:08:14 +0100

    added word component logic and styling

--- src/components/Word.tsx
+++ src/components/Word.tsx
@@ -1,5 +1,39 @@
-import React from "react"
+import styled from "styled-components"
 
-export default function Word() {
-  return <div>Word</div>
+type Props = {
+  wordToGuess: string
+  guessedLetters: string[]
 }
+
+export default function Word({ wordToGuess, guessedLetters }: Props) {
+  return (
+    <WordContainer>
+      {wordToGuess.split("").map((letter, index) => {
+        return (
+          <span className="letter" key={index}>
+            <span
+              style={{
+                visibility: guessedLetters.includes(letter) ? "visible" : "hidden",
+              }}
+            >
+              {letter}
+            </span>
+          </span>
+        )
+      })}
+    </WordContainer>
+  )
+}
+
+const WordContainer = styled.div`
+  display: flex;
+  gap: 0.25em;
+  font-size: 6rem;
+  font-weight: bold;
+  text-transform: uppercase;
+  font-family: monospace;
+
+  .letter {
+    border-bottom: 0.1em solid black;
+  }
+`
